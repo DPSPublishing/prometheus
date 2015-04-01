c69aab4b599430b55ee7a927507b032fff70b99c
Thomas Albrighton <thomasalbrighton@gmail.com>
2015-04-01 21:22:59 +1030

    testing updates

---
+++ scripts/deploy.js
@@ -0,0 +1,36 @@
+module.exports = function(robot) {
+    robot.router.post('/deploy/:site', function(req, res) {
+        var site = req.params.site;
+        var data = {commits:[]};
+        if (req.body.payload) {
+            data = JSON.parse(req.body.payload);
+        }
+        var users = [];
+
+        if (data.commits) {
+            for (j = 0, len = data.commits.length; j < len; j++) {
+                commit = data.commits[j];
+                if (users.indexOf(commit.author) === -1) {
+                    users.push(commit.author);
+                }
+            }
+        }
+
+
+        users_string = users.join('\n')
+
+        robot.emit('slack.attachment',{
+            content: {
+                // see https://api.slack.com/docs/attachments
+                text: "New commits from " + users_string,
+                fallback: "New commits from " + users_string,
+                fields: [{
+                    title: "Field title",
+                    value: "Field value"
+                }]
+            },
+            channel: "#deployment"
+        });
+
+    });
+}
