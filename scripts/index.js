9a2634d5647d06c334e0299b9d168d2fc5c81d21
Thomas Albrighton <thomasalbrighton@gmail.com>
2015-04-02 21:12:51 +1030

    fixed wrong view path

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ Handlebars = require('handlebars');
 module.exports = function(robot) {
 	robot.router.get('/', function(req, res) {
 
-		var template = require('./build/js/templates.js');
+		var template = require('./../build/js/templates.js');
 
 		res.send(JSON.stringify(template))
 
